8d087a9c02ac9fe4a4b8cba3b2f357397c76d3bf
SumeyyeSuslu <suslu.sumeyye@gmail.com>
2017-08-14 16:42:01 -0500

    Create extractingvalues.js

---
+++ extractingvalues.js
@@ -0,0 +1,23 @@
+//ES6
+const point = [10, 25, -34];
+const x = point[0];
+const y = point[1];
+const z = point[2];
+console.log(x,y,z);
+
+//Output:
+10
+25
+-34
+
+//Babel - ES5
+"use strict";
+
+var point = [10, 25, -34];
+var x = point[0];
+var y = point[1];
+var z = point[2];
+console.log(x, y, z);
+
+//Output:
+10 25 -34
