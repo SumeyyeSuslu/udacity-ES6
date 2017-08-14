c7ac8f97cf8e4606c8153b2bae2a81d9b068d4ed
SumeyyeSuslu <suslu.sumeyye@gmail.com>
2017-08-14 15:54:01 -0500

    Create let-var.js

---
+++ let-var.js
@@ -0,0 +1,16 @@
+//ES6:
+let instructor = 'James';
+instructor = 'Richard';
+console.log(instructor);
+//Output:
+//"Richard"
+
+//Babel
+'use strict';
+
+var instructor = 'James';
+instructor = 'Richard';
+console.log(instructor);
+
+//Output:
+//Richard
