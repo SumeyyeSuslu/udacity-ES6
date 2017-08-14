8b495050520f9bbfadb7504db5b4f386e797215a
SumeyyeSuslu <suslu.sumeyye@gmail.com>
2017-08-14 16:25:06 -0500

    Create templateliterals2.js

---
+++ templateliterals2.js
@@ -0,0 +1,31 @@
+//ES6:
+/*
+* Instructions: Change the `greeting` string to use a template literal
+*/
+const myName = '[Sumeyye]';
+const greeting = 'Hello, my name is ' + myName;
+console.log(greeting);
+
+//Output:
+// "Hello, my name is [Sumeyye]"
+
+//Backtick-ES6:
+/*
+* Instructions: Change the `greeting` string to use a template literal
+*/
+const myName = '[Sumeyye]';
+const greeting = `Hello, my name is ${myName}`;
+console.log(greeting);
+
+//Output:
+// "Hello, my name is [Sumeyye]"
+
+//Babel-ES5
+'use strict';
+
+/*
+* Instructions: Change the `greeting` string to use a template literal
+*/
+var myName = '[Sumeyye]';
+var greeting = 'Hello, my name is ' + myName;
+console.log(greeting);
