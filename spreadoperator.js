51beeb60c867a42a41d9eca6c5ef0048fa454a20
SumeyyeSuslu <suslu.sumeyye@gmail.com>
2017-08-14 20:42:00 -0500

    Update spreadoperator.js

--- spreadoperator.js
+++ spreadoperator.js
@@ -19,4 +19,34 @@ var books = ["Don Quixote", "The Hobbit", "Alice in Wonderland", "Tale of Two Ci
 (_console = console).log.apply(_console, books);
 
 //Output:
+/*
 "Don Quixote" "The Hobbit" "Alice in Wonderland" "Tale of Two Cities"
+*/
+//ES6
+const primes = new Set([2,3,5,7,11,13,17,1923,29]);
+console.log(...primes);
+
+//Output:
+/*
+2
+3
+5
+7
+11
+13
+17
+1923
+29
+*/
+
+"use strict";
+
+var _console;
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+var primes = new Set([2, 3, 5, 7, 11, 13, 17, 1923, 29]);
+(_console = console).log.apply(_console, _toConsumableArray(primes));
+
+//Output:
+2 3 5 7 11 13 17 1923 29
