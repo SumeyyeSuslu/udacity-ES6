ddb0778e884bf0ac3f804400b70e9b554ae57c09
SumeyyeSuslu <suslu.sumeyye@gmail.com>
2017-08-14 20:51:14 -0500

    Update combiningarrywithconcat.js

--- combiningarrywithconcat.js
+++ combiningarrywithconcat.js
@@ -28,3 +28,37 @@ Array [
   "carrots"
 ]
 */
+// Warning: ES6:
+const fruits = ["apples", "bananas","pears"];
+const vegetables = ["corn","potatoes","carrots"];
+const produce = [fruits,vegetables];
+console.log(produce);
+
+//Output:
+/*
+[["apples", "bananas", "pears"], ["corn", "potatoes", "carrots"]]
+*/
+
+//ES5:
+"use strict";
+
+var fruits = ["apples", "bananas", "pears"];
+var vegetables = ["corn", "potatoes", "carrots"];
+var produce = [fruits, vegetables];
+console.log(produce);
+
+//Output:
+/*
+Array [
+  Array [
+    "apples",
+    "bananas",
+    "pears"
+  ],
+  Array [
+    "corn",
+    "potatoes",
+    "carrots"
+  ]
+]
+*/
