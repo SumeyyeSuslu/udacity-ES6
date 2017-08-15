15cc1523f5d1bf86ae8de12baead1d36a23decef
SumeyyeSuslu <suslu.sumeyye@gmail.com>
2017-08-14 19:09:12 -0500

    Create adv.forofloop.js

---
+++ adv.forofloop.js
@@ -0,0 +1,73 @@
+//ES6:
+//You don't have to worry about adding new properties to objects.The for...of loop will only loop over the values in the object.
+Array.prototype.decimalfy=function(){
+  for (let i=0; i<this.length; i++){
+    this[i] = this[i].toFixed(2);
+  }
+};
+const digits = [0,1,2,3,4,5,6,7,8,9];
+for (const digit of digits){
+  console.log(digit);
+}
+
+//Output:
+/*
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+*/
+
+//Babel -ES5:
+"use strict";
+
+Array.prototype.decimalfy = function () {
+  for (var i = 0; i < this.length; i++) {
+    this[i] = this[i].toFixed(2);
+  }
+};
+var digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var _iteratorNormalCompletion = true;
+var _didIteratorError = false;
+var _iteratorError = undefined;
+
+try {
+  for (var _iterator = digits[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+    var digit = _step.value;
+
+    console.log(digit);
+  }
+} catch (err) {
+  _didIteratorError = true;
+  _iteratorError = err;
+} finally {
+  try {
+    if (!_iteratorNormalCompletion && _iterator.return) {
+      _iterator.return();
+    }
+  } finally {
+    if (_didIteratorError) {
+      throw _iteratorError;
+    }
+  }
+}
+
+//Output:
+/*
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+*/
